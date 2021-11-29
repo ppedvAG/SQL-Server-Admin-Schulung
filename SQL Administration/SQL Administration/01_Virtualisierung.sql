9a43ea4a07445caa5520242a34f6cebf1024acbc
Rauchendraser <andreasr@ppedv.de>
2021-11-29 10:44:52 +0100

    VM - Setup

---
+++ SQL Administration/SQL Administration/01_Virtualisierung.sql
@@ -0,0 +1,37 @@
+/*
+
+RAM HDD CPU
+
+HDD entlasten entlasten!!
+
+-->RAM
+-->CPU
+
+bei Virt.. ist auch die Hardware gleich oder besser als vorher..
+
+			RAM		Socket			Kerne
+			16GB	  1					4
+			16GB	  1					8
+
+			Für Virt: min 4 GB --> 12 GB RAM
+
+
+HV-DC:    2048 max     1               2
+HV_SQL1:  6000 fix	   1               4(8)
+HV-SQL2:  4000 fix     1			   4(8)
+
+
+
+NUMA-- ZUordnung von Socket zu RAM Slots..
+
+Vorsicht bei Migration.. evtl Änderung
+
+
+
+
+
+
+
+
+
+*/
---
+++ SQL Administration/SQL Administration/02_Installation.sql
@@ -0,0 +1,126 @@
+/*
+
+
+
+Dienstkonten
+
+SQL Server (DB Engine)
+
+SQL Agent( Jobs)
+
+SQL Volltextsuche ( Suche "Maier", Meier meir mayer)
+
+SQL Browser
+
+
+NT Service\MSSQL$FE
+
+Was macht das NT Service:
+autom Kennwortänderungen ..cool
+lok Konten --> \\Backup  ?  ..uncool --> Computerkonto!
+
+Volumewartungstask
+kein Ausnullen bei Vergrößerungen
+
+
+
+Anmeldung
+
+HV-SQL2
+
+schulung\Administrator
+ppedv2019!
+
+
+Sicherheit:
+
+Gemischte Auth (NT + SQL Logins)
+--> Konto: SA aktiv  (Kennwort 14 Zeiechen komplex.)
+	--später deaktvieren--> Ersatzkonto
+
+--> NT Admins sind keine SQL Admins!
+--manuell jemand als Admin hinzufügen (user oder Gruppen möglich)
+
+
+
+Pfaden
+
+DB Datendateien
+DB Logfiles
+
+Regel: Trenne Daten von Logfiles physikalisch
+
+Backup...
+
+
+Binärkram: egal.. c:\Program Files\...
+
+
+
+TempDB
+
+eig HDDs für MDF und LDF
+
+
+
+RAM
+cool.. SQL Server max Arbeitsspeicher sollte auf einen Wert beschränbkt werden
+damit das OS noch Platz zum Arbeiten besitzt.
+--> Gesmat - OS
+
+
+MAXDOP _ 
+wieviele CPUs verwenden Abfragen
+default: 0 ..sprich 1 oder alle
+Regel: 1 oder alle (max 8)
+
+
+
+INSTANZ
+..kann auf einem Rechner 50 mal installiert werden (supportet)
+
+--> Std Instanz:		"PC25"   --- PC25
+
+-- benannt Instanz:                  PC25\INSTANZNAME
+
+Instanzen sind total isoliert voneinander
+eig Prozesse eig SystemDB
+
+auch verschd Versionen möglich
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
